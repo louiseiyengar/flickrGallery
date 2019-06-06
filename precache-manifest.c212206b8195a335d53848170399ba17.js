e50c0b999bd6809b3395da109433701310fc9f5c
Louise Iyengar <lki@louiseiyengar.com>
2019-06-06 16:27:44 -0400

    Updates

---
+++ precache-manifest.c212206b8195a335d53848170399ba17.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "943d4a3e7279ae0574f2bf850080dcef",
+    "url": "/flickrGallery/index.html"
+  },
+  {
+    "revision": "263b3fdaa7de4b487805",
+    "url": "/flickrGallery/static/css/main.4d5bc147.chunk.css"
+  },
+  {
+    "revision": "cd825ae6ed8f8b114b20",
+    "url": "/flickrGallery/static/js/2.8ef42797.chunk.js"
+  },
+  {
+    "revision": "263b3fdaa7de4b487805",
+    "url": "/flickrGallery/static/js/main.6a4786a2.chunk.js"
+  },
+  {
+    "revision": "5acefb3826b840213238",
+    "url": "/flickrGallery/static/js/runtime~main.efa1ef29.js"
+  }
+]);
